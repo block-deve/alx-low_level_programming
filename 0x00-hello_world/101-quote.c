2d90a1843f4cd339adb9f5043475daf3367479a9
Benyahia <eloualid.benyahia@gmail.com>
2023-03-16 03:52:31 -0700

    print to stderr with fprint

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" \-\ Dora Korpar\, 2015\-10\-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
