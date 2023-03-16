c0b9ea2b0b37847d525d03dea7e18842cc183fbb
Benyahia <eloualid.benyahia@gmail.com>
2023-03-16 02:46:08 -0700

    adding the betty style and rmoving 2 new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*this c program print to the io a string with puts function*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
