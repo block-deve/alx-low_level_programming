7ba3f0b5ac775283a1d03eadfa2959d7d24e79b4
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 16:58:43 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,7 @@ int print_last_digit(int i)
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
-		_putchar(last_digit + '0');
-		_putchar(last_digit + '0');
 	}
-	return (last_digit);
+		_putchar(last_digit + '0');
+		return (last_digit);
 }
