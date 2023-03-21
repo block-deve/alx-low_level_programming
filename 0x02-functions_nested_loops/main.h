f6fe6564bdfd30d3ee5c1a86611bbb485723e4be
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 09:04:26 -0700

    trying new solution for multiple definitions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,9 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_alphabet - writes lower case alphabet to stdo
