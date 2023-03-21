b006d5e7d06db22aaa7c2eb36ccdeca4d6e2ba33
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 12:35:05 -0700

    is lower first test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-#include <ctype.h>
 #include "main.h"
 /**
  *_islower - check a char if lower case.
@@ -10,8 +9,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
-	else if (c >= 65 && c <= 90)
-		return (1);
 	else
 		return (0);
+	_putchar('\n');
 }
