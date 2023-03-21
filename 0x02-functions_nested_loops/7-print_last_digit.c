9f21efbd694d588568d499509a39b76202026605
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 16:17:12 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Computes the last digit of given number
+ * @i: The integer to be computed.
+ *
+ * Return: The last digit of the integer.
+ */
+int print_last_digit(int i)
+{
+	return ((i % 10));
+
+}
