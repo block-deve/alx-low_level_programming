89fa27607b1daa86d25668fba8b36462fc12eb4f
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 16:07:54 -0700

    modulo of i

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - give the absolut value of an int
+ *@i: value to give modulo of it
+ *Return: Always 0.
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
