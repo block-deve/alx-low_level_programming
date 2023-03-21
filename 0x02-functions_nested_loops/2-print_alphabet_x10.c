9e5e3ee1b6862adddd0b090f903142f04fb469db
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 11:35:02 -0700

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,11 +15,11 @@ void print_alphabet_x10(void)
 		char ch[28] = "abcdefghijklmnopqrstuvwxyz\n";
 		int j = 0;
 
-		while (ch[j] != '\0' && j = i)
+		while (ch[j] != '\0')
 		{
 			_putchar(ch[j]);
+			j++;
 		}
-		j++;
-	}
 	i++;
+	}
 }
