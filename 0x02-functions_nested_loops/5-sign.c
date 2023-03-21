e6566de25493609707c93abe9e33253297a24a67
Benyahia <eloualid.benyahia@gmail.com>
2023-03-21 15:14:05 -0700

    checking sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - print int sign
+ *@n: int to check sign for
+ *
+ * Return: Always 1 if positiv 0 if null -1 if negatif.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	else if (n == 0)
+		_putchar('0');
+	return (0);
+	else
+		(n < 0)
+	{
+		_putchar('-');
+		_putchar('1');
+		return (-1);
+	}
+}
