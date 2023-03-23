3768e4334e79acf51235007e64d500d9cc44d074
Benyahia <eloualid.benyahia@gmail.com>
2023-03-23 01:14:08 -0700

    printing digits with _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_numbers - print digits
+*
+*Return:
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	_putchar('\n');
+}
