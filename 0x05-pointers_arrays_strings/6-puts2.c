958f336352abdd4af42d0d6bf27d995ae392c28e
Benyahia <eloualid.benyahia@gmail.com>
2023-03-28 05:12:45 -0700

    print odde in a string wi modulo

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string,
+ *starting with the first character, followed by a new line.
+ *
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2  == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
