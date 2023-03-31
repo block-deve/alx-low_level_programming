5ee6596ab1df18d9af3ca652a201422b326178cb
Benyahia <eloualid.benyahia@gmail.com>
2023-03-30 18:02:37 -0700

    function that compares tow strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strcmp - a function that compares two strings
+*
+*@s1: char pointer nbr 1 string array
+*@s2: char pointer nbr 2 string array
+*
+*Return: int number of differences in the string
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0') && (*s2 != '\0') && (s1 == s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+		return (*s1 - *s2);
+}
